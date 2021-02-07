08399dff6f789cc02a4ab6ee0452f4a6156e5c33
Nicolas Guarini <nikiguarincia@gmail.com>
2021-02-07 16:13:01 +0100

    Index page created

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
-//import Logo from './logo.svg'
 import Helmet from 'react-helmet'
+import './css/Home.css'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     return(
@@ -12,9 +13,22 @@ const Home = () => {
                     content="'DRUMS MAKER' is a web-app where you can create and manage simple drum patterns. Let's start now!"
                 />
             </Helmet>
-            {
-             //   <img src={Logo} alt="logo"></img>
-            }
+            
+            <div className="container">
+                <div className="nav">
+                    <span className="nav-about">about</span>
+                    <span className="nav-create">create drums</span>
+                </div>
+
+                <div className="hero">
+                    <h1>Drums Maker</h1>
+                    <p>by <a href="https://nicolasguarini.it" target="_blank" rel="noopener noreferrer" style={{color: "black"}}>Nicolas Guarini</a></p>
+                    <Link to="/create" className="hero-btn">Let's start now!</Link>
+                </div>
+            </div>
+            
+            
+
         </>
     )
 }
